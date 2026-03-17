05e8b3e0d355afee87e2b9ccb3cf32bf9076cd59
siddartha reddy baradi <siddartha.baradi@zemosolabs.com>
2026-03-17 08:44:08 +0530

    added MongoDB with ConfigMap and Secret

--- app/app.js
+++ app/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const PORT = 3000;
+
+const dbUser = process.env.DB_USERNAME;
+const dbPass = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
+const mongoURI = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017/${dbName}?authSource=admin`;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send(`
         <h1>Kubernetes DevOps Demo</h1>
-        <p>Application running inside Docker container</p>
-        <p>Version: 1.0</p>
+        <p>Application running inside Kubernetes</p>
+        <p>Database: MongoDB Connected</p>
     `);
-});
-
-app.get("/health", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "Application is healthy",
-  });
 });
 
 app.listen(PORT, () => {
